5ecb7258444b92c886a20a42b1fef7ca0c04f5ad
ShadowGirl01 <2005vashu@gmail.com>
2026-07-21 19:45:21 +0530

    implemented bookcard page

--- components/ui/BookCard.tsx
+++ components/ui/BookCard.tsx
@@ -5,23 +5,22 @@ import React from "react";
 
 const BookCard = ({ title, author, coverURL, slug }: BookCardProps) => {
   return (
-    
-      <Link href={`/books/${slug}`}>
-        <article className="book-card">
-          <figure className="book-card-figure">
-            <div className="book-card-cover-wrapper">
-              <Image src={coverURL} alt={title} width={133}
-              height={200} className="book-card-cover"  />
-            </div>
-          </figure>
 
+    <Link href={`/books/${slug}`}>
+      <article className="book-card">
+        <figure className="book-card-figure">
+          <div className="book-card-cover-wrapper">
+            <Image src={coverURL} alt={title} width={133}
+              height={200} className="book-card-cover" />
+          </div>
           <figcaption className="book-card-meta">
             <h3 className="book-card-title">{title}</h3>
             <p className="book-card-author">{author}</p>
           </figcaption>
-        </article>
-      </Link>
-    
+        </figure>
+      </article>
+    </Link>
+
   );
 };
 
